2fc17714c7da5248c42b5f56ea3f1d24d736ab13
kanthTs <tsiddakanth@gmail.com>
2025-10-18 22:12:27 +0530

    task1 completed

--- src/test/java/com/jpmc/midascore/FileLoader.java
+++ src/test/java/com/jpmc/midascore/FileLoader.java
@@ -1,19 +1,25 @@
 package com.jpmc.midascore;
 
 import org.springframework.stereotype.Component;
-import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FileLoader {
+
     public String[] loadStrings(String path) {
-        try {
-            InputStream inputStream = this.getClass().getResourceAsStream(path);
-            String fileText = IOUtils.toString(inputStream, "UTF-8");
+        try (InputStream inputStream = this.getClass().getResourceAsStream(path)) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: " + path);
+            }
+            String fileText = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
             return fileText.split(System.lineSeparator());
         } catch (Exception e) {
+            e.printStackTrace(); // helps with debugging
             return null;
         }
     }
 }
+
